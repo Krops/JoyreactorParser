1d1a717f2e35c17f9f47b51f119f0e07e5272f46
Andrii Kropyva <akropyva@lohika.com>
2022-07-11 23:39:12 +0300

    Add scheduler

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import configparser
 import json
 
 import telebot
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from telebot.types import InputMediaPhoto, InputMediaVideo
 
 from parser import get_m
@@ -9,44 +11,61 @@ from parser import get_m
 config = configparser.ConfigParser()
 config.read('config.ini')
 token = ''
-if 'token' in config:
-    token = config['token']
+if 'AUTH' in config:
+    token = config['AUTH']['token']
 bot = telebot.TeleBot(token)
 url = 'http://joyreactor.cc'
-result = get_m()
-print(result)
-saved_keys_file = open('dict.json', "r")
-saved_keys_json = set(json.load(saved_keys_file))
-json_keys = json.dumps(list(result.keys()))
-saved_keys_file = open('dict.json', "w")
-saved_keys_file.write(json_keys)
-diff = set(result.keys()) - saved_keys_json
-for g in diff:
-    item_list = []
-    text = ""
-    url_text = f"{url}{g}"
-    for item in result.get(g):
-        if 'jpeg' in item:
-            media = InputMediaPhoto(media=item)
-            item_list.append(media)
-        elif 'png' in item:
-            media = InputMediaPhoto(media=item)
-            item_list.append(media)
-        elif 'mp4' in item:
-            media = InputMediaVideo(media=item)
-            item_list.append(media)
-        elif 'gif' in item:
-            media = InputMediaVideo(media=item)
-            item_list.append(media)
+
+
+def check_sent_messages(result):
+    saved_keys_file = open('dict.json', "r")
+    saved_keys_json = set(json.load(saved_keys_file))
+    json_keys = json.dumps(list(result.keys()))
+    saved_keys_file = open('dict.json', "w")
+    saved_keys_file.write(json_keys)
+    diff = set(result.keys()) - saved_keys_json
+    return diff
+
+
+def send_message(result, diff):
+    for g in diff:
+        item_list = []
+        text = ""
+        url_text = f"{url}{g}"
+        for item in result.get(g):
+            if 'jpeg' in item:
+                media = InputMediaPhoto(media=item)
+                item_list.append(media)
+            elif 'png' in item:
+                media = InputMediaPhoto(media=item)
+                item_list.append(media)
+            elif 'mp4' in item:
+                media = InputMediaVideo(media=item)
+                item_list.append(media)
+            elif 'gif' in item:
+                media = InputMediaVideo(media=item)
+                item_list.append(media)
+            else:
+                text = text + " " + item
+        if len(item_list) > 0:
+            if len(text) > 1024:
+                bot.send_message('@joyreact_channel', text=text, disable_web_page_preview=True)
+                item_list[0].caption = url_text
+            else:
+                item_list[0].caption = text + '\n' + url_text
+            bot.send_media_group('@joyreact_channel', item_list)
         else:
-            text = text + " " + item
-    if len(item_list) > 0:
-        if len(text) > 1024:
+            text = text + '\n' + url_text
             bot.send_message('@joyreact_channel', text=text, disable_web_page_preview=True)
-            item_list[0].caption = url_text
-        else:
-            item_list[0].caption = text + '\n' + url_text
-        bot.send_media_group('@joyreact_channel', item_list)
-    else:
-        text = text + '\n' + url_text
-        bot.send_message('@joyreact_channel', text=text, disable_web_page_preview=True)
+
+
+scheduler = BlockingScheduler()
+
+
+@scheduler.scheduled_job(IntervalTrigger(minutes=10))
+def some_job():
+    result = get_m()
+    send_message(result, check_sent_messages(result))
+
+
+scheduler.start()
